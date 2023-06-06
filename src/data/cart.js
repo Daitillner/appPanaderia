a969ad2662f664539d4bf0bc7487dcbabf558999
daianatillner <tillnerdaiana@gmail.com>
2023-06-05 21:27:16 -0300

    modificaciones

---
+++ src/data/cart.js
@@ -0,0 +1,22 @@
+export const CART = [
+
+{
+    id: "1",
+    category: "1",
+    name: "Baguette",
+    description: "Clasico pan frances", 
+    weight: "350gr",
+    price: 16,
+    quabtity: 3,
+},
+{
+    id: "2",
+    category: "2",
+    name: "Pan Cibatta Blanco",
+    description: "Pan de alta hidratacion, miga alveorada y corteza crujiente", 
+    weight: "800gr",
+    price: 30,
+    quantity: 2, 
+},
+    
+]
